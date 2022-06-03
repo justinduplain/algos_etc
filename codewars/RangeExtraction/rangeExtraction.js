352dc50ca96fa8ee90c08921b7f0b12bb8f36fb4
pmentropy <justin.duplain@gmail.com>
2022-06-03 14:44:59 -0400

    submitted version

--- codewars/RangeExtraction/rangeExtraction.js
+++ codewars/RangeExtraction/rangeExtraction.js
@@ -11,51 +11,80 @@ solution([-10, -9, -8, -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 1
 // returns "-10--8,-6,-3-1,3-5,7-11,14,15,17-20"
 */
 
+// const solution = array => {
+//   //x points to the low value in a range
+//   //a & b are used as pointers as we stetp through the array
+//   //result holds the output
+//   let [x, a, b, result] = [0, 0, 1, []]
+//   //helper function to update our pointers
+//   const updatePointers = () => {
+//     a = b
+//     x = b
+//     b++
+//   }
+
+//   //run through the array from the left and stop at the end
+//   while (a < array.length) {
+//     /* If x and a are pointing at the same index, then we haven't
+//     started evaluating a range *yet* */
+//     if (x === a) {
+//       //if the right value is more than one higher than left
+//       //are not evaluating a range.
+//       if (array[b] - array[a] !== 1) {
+//         //Add the left number to the result array.
+//         result.push(array[x])
+//         updatePointers()
+//       }
+//       //if the right value is one number higher than left we MAY be evaluating a range. X stays the same as we evaluate
+//       if (array[b] - array[a] === 1) {
+//         a++
+//         b++
+//         /* at this point we know that we are trying to evaluate
+//         a range. if the right value is again one higher, we
+//         know the range is more than two increments so we will
+//         iterate through the range. */
+//         if (array[b] - array[a] === 1) {
+//           while (array[b] - array[a] === 1) {
+//             a++
+//             b++
+//           }
+//           result.push(array[x] + '-' + array[a])
+//           updatePointers()
+//         } else {
+//           result.push(array[x], array[a])
+//           updatePointers()
+//         }
+//       }
+//     }
+//   }
+//   return result.join(',')
+// }
+
 const solution = array => {
-  //x points to the low value in a range
-  //a & b are used as pointers as we stetp through the array
-  //result holds the output
   let [x, a, b, result] = [0, 0, 1, []]
-  //helper function to update our pointers
-  const updatePointers = () => {
-    a = b
-    x = b
+
+  const incrimentAB = () => {
+    a++
     b++
   }
 
-  //run through the array from the left and stop at the end
   while (a < array.length) {
-    /* If x and a are pointing at the same index, then we haven't
-    started evaluating a range *yet* */
-    if (x === a) {
-      //if the right value is more than one higher than left
-      //are not evaluating a range.
-      if (array[b] - array[a] !== 1) {
-        //Add the left number to the result array.
-        result.push(array[x])
-        updatePointers()
-      }
-      //if the right value is one number higher than left we MAY be evaluating a range. X stays the same as we evaluate
+    if (array[b] - array[a] !== 1) {
+      result.push(array[x])
+    } else {
+      incrimentAB()
       if (array[b] - array[a] === 1) {
-        a++
-        b++
-        /* at this point we know that we are trying to evaluate
-        a range. if the right value is again one higher, we
-        know the range is more than two increments so we will
-        iterate through the range. */
-        if (array[b] - array[a] === 1) {
-          while (array[b] - array[a] === 1) {
-            a++
-            b++
-          }
-          result.push(array[x] + '-' + array[a])
-          updatePointers()
-        } else {
-          result.push(array[x], array[a])
-          updatePointers()
+        while (array[b] - array[a] === 1) {
+          incrimentAB()
         }
+        result.push(array[x] + '-' + array[a])
+      } else {
+        result.push(array[x], array[a])
       }
     }
+    a = b
+    x = b
+    b++
   }
   return result.join(',')
 }
