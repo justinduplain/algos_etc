bae54f9cfe163d55392916b19ee53c3d2c0148f0
pmentropy <justin.duplain@gmail.com>
2022-07-16 14:10:39 -0400

    non optimal Roman Numberals Encoder solution

--- codewars/romanNumeralsHelper.js
+++ codewars/romanNumeralsHelper.js
@@ -17,14 +17,19 @@ class RomanNumerals {
   constructor() {}
 }
 
-RomanNumerals.toRoman = (number) => "M";
+RomanNumerals.toRoman = number => {
+  let [...numStrArr] = JSON.stringify(number)
+  console.log(numStrArr)
+  let result = []
+  let j = 0
+  for (let i = numStrArr.length - 1; i >= 0; i--) {
+    result.unshift(lookup(numStrArr[i], j))
+    j++
+  }
+  return result.join('')
+}
 
-//example 1666 -> 6, 60, 600 1000
-//right most char is 6, so this is represented as VI
-//next input is 60, which is depicted as LX
-//next input is 600, which is depicted as DC,
-
-RomanNumerals.fromRoman = (string) => {
+RomanNumerals.fromRoman = string => {
   const translator = {
     I: 1,
     IV: -1,
@@ -39,10 +44,60 @@ RomanNumerals.fromRoman = (string) => {
     CD: -100,
     D: 500,
     M: 1000,
-  };
+  }
   return Array.from(string).reduce((totaller, r, i, arr) => {
     return translator.hasOwnProperty(`${r}${arr[i + 1]}`)
       ? (totaller += translator[`${r}${arr[i + 1]}`])
-      : (totaller += translator[`${r}`]);
-  }, 0);
-};
+      : (totaller += translator[`${r}`])
+  }, 0)
+}
+
+const lookup = (numChar, place) => {
+  switch (place) {
+    case 1:
+      numChar = [numChar, '0'].join('')
+      break
+    case 2:
+      numChar = [numChar, '00'].join('')
+      break
+    case 3:
+      numChar = [numChar, '000'].join('')
+      break
+  }
+  return map[numChar]
+}
+
+const map = {
+  1: 'I',
+  2: 'II',
+  3: 'III',
+  4: 'IV',
+  5: 'V',
+  6: 'VI',
+  7: 'VII',
+  8: 'VIII',
+  9: 'IX',
+  10: 'X',
+  20: 'XX',
+  30: 'XXX',
+  40: 'XL',
+  50: 'L',
+  60: 'LX',
+  70: 'LXX',
+  80: 'LXXX',
+  90: 'XC',
+  100: 'C',
+  200: 'CC',
+  300: 'CCC',
+  400: 'CD',
+  500: 'D',
+  600: 'DC',
+  700: 'DCC',
+  800: 'DCCC',
+  900: 'CM',
+  1000: 'M',
+  2000: 'MM',
+  3000: 'MMM',
+  4000: 'MMMM',
+  5000: 'V',
+}
